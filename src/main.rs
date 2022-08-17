f63e0b70056147fb42924dde596afa0d035d6e1f
David Fernández <david.fernandez@idiada.com>
2022-08-17 18:51:13 +0200

    Tuple & destructuring example

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@
 
 fn main() {
     println!("Hello World!");
+    let location: (&str,f64,f64) = ("Home",41.128639706121255, 1.244442255677015);
+
+    //destructuring the tuple (also can be done with arrays)
+    let (name,latitude,longitude) = location;
+    
+    println!("Location name: {}, latitude {}, longitude {}",name,latitude,longitude);
 }
