ea48e768b2bd941181983797838fa9399fd1b601
David Fernández <david.fernandez@idiada.com>
2022-08-18 11:14:38 +0200

    option example

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,14 @@ fn main() {
     slogan.push(' ');
     slogan = slogan + "fucking amazing.";
     println!("{}",slogan);
+
+
+    let phrase=String::from("Duck Airlines");
+    let letter =phrase.chars().nth(20);
+
+    let value = match letter{
+        Some(character) => character.to_string(),
+        None=> String::from("No value")
+    };
+    println!("{}",value);
 }
